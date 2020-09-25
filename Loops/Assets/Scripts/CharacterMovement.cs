9998a1515bafaf800767eb3646f0fe62b1ca8adc
Cory Reardon <cjreardon@gmail.com>
2020-09-25 10:51:53 -0700

    WIP: Character movement

---
+++ Loops/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float turnSpeed = 20f;
+    Vector3 mMovement;
+
+    Quaternion m_Rotation = Quaternion.identity;
+
+
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        mMovement.Set(horizontal, 0f, vertical);
+        mMovement.Normalize();
+
+        transform.position += mMovement * Time.deltaTime * moveSpeed;
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void Update()
+    {
+
+    }
+}
