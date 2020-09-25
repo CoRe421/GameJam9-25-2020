493158e33a61d4c7030c093a654ecbc2381957b9
Cory Reardon <cjreardon@gmail.com>
2020-09-25 12:05:44 -0700

    Changes to camera

--- Loops/Assets/Scripts/CharacterMovement.cs
+++ Loops/Assets/Scripts/CharacterMovement.cs
@@ -8,8 +8,6 @@ public class CharacterMovement : MonoBehaviour
     public float turnSpeed = 20f;
     Vector3 mMovement;
 
-    Quaternion m_Rotation = Quaternion.identity;
-
 
     void Start()
     {
@@ -22,12 +20,17 @@ public class CharacterMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         mMovement.Set(horizontal, vertical, 0f);
-        mMovement.Normalize();
 
         transform.position += mMovement * Time.deltaTime * moveSpeed;
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, mMovement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation(desiredForward);
+        /**
+        float rotation = -horizontal * turnSpeed;
+        transform.Rotate(transform.forward * rotation);
+        **/
+
+        float rotation = -horizontal * turnSpeed;
+        transform.Rotate(Vector3.forward * rotation);
+
     }
 
     void Update()
