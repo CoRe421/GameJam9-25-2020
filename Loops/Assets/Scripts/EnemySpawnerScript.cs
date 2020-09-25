5c16d90f86010594926ddfa0507b5b4adcf47def
Nicholas <nicholaschan052@gmail.com>
2020-09-25 11:36:07 -0700

    Apple Spawner

---
+++ Loops/Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    float randY;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0;
+
+    void Update() {
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-16f, -6f);
+            randY = Random.Range(5f, -4.25f);
+            whereToSpawn = new Vector2(randX, randY);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
