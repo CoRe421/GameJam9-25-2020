44146344f07e3aa6957c849a6c6b67d47c23ee3f
Cory Reardon <cjreardon@gmail.com>
2020-09-25 17:35:59 -0700

    Created triggers on bottom walls

---
+++ Loops/Assets/AppleCatcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleCatcher : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "FallingApple")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
